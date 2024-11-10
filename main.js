15ebe1cad0daaa78d9d2ae5fbd36e4ba7d48a848
Suyeong <81769862+su794@users.noreply.github.com>
2024-11-10 12:23:28 +0900

    refactoring js file

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
+'use strict';
+
+const farmArea = document.querySelector('#farmArea');
+const farmAreaRect = farmArea.getBoundingClientRect();
+const CARROT_SIZE = 50;
+const CARROT_COUNT = 10;
+const BUG_COUNT = 5;
+let gameOn = false;
+
+const playPauseBtn = document.querySelector('.play-pause');
+const playBtn = document.querySelector('.fa-play');
+const pauseBtn = document.querySelector('.fa-pause');
+const rePlayBtn = document.querySelectorAll('.re-try i');
+
+let score = document.querySelector('.carrotNum');
 let timer = document.querySelector('.timer');
 let time = 10;
 let timeleft = time;
 let downloadTimer;
 let timeStr = timeleft.toString();
 
-const playPauseBtn = document.querySelector('.play-pause');
-const playBtn = document.querySelector('#play');
-const pauseBtn = document.querySelector('#pause');
-const rePlayBtn = document.querySelectorAll('.re-try i');
-
-const farmArea = document.querySelector('#farmArea');
-
 const failedMsg = document.querySelector('.failed');
 const winMsg = document.querySelector('.won');
 
-function randomCorX(min, max) {
-  return Math.floor(Math.random() * (max - min));
+function randomCor(min, max) {
+  // this includes the min value ( + min at the end )
+  return Math.floor(Math.random() * (max - min) + min);
 }
-
-let results = [];
 
 let carrotLength = document.querySelectorAll('.carrot').length;
 
@@ -26,10 +33,10 @@ farmArea.addEventListener('click', (e) => {
   
   if(e.target.classList.contains('bug')) {
     stopGame('bug');
-  } else if (e.target.classList.contains('carrot')) {
+  } else if (e.target.buttonIconclassList.contains('carrot')) {
     e.target.classList.add('hidden');
     let carrotLength = document.querySelectorAll('.carrot:not(.hidden)').length;
-    document.querySelector('.carrotNum').innerText = carrotLength;
+    score.innerText = carrotLength;
     
     if( carrotLength === 0 ) {
       stopGame('win');
@@ -38,43 +45,69 @@ farmArea.addEventListener('click', (e) => {
 })
 
 function initGame(e) {
-  console.log(e.target)
-  timeleft = 10;
-    downloadTimer = undefined;
-    document.querySelector('body').classList.add('gameOn');
-    pauseBtn.classList.remove('hidden');
-    playBtn.classList.add('hidden');
-    document.querySelector('.message').classList.add('hidden');
-    document.querySelectorAll('.re-try').forEach( btn => btn.classList.remove('hidden') );
-    document.querySelector('.carrotNum').innerText = carrotLength;
-    timer.innerHTML = `00:${timeStr.padStart(2, '0')}`;
-    startTimer();
+  // timeleft = 10;
+  //   downloadTimer = undefined;
+  //   document.querySelector('body').classList.add('gameOn');
+  //   pauseBtn.classList.remove('hidden');
+  //   playBtn.classList.add('hidden');
+  //   document.querySelector('.message').classList.add('hidden');
+  //   document.querySelectorAll('.re-try').forEach( btn => btn.classList.remove('hidden') );
+  //   document.querySelector('.carrotNum').innerText = carrotLength;
+  //   timer.innerHTML = `00:${timeStr.padStart(2, '0')}`;
+  //   startTimer();
   
     
-    for (let i = 0; i < 17; i++) {
-      let xCoor = randomCorX(farmArea.getBoundingClientRect().x, farmArea.getBoundingClientRect().width);
-      let yCoor = randomCorX(farmArea.getBoundingClientRect().top, farmArea.getBoundingClientRect().bottom);
-      
-      let elemCoor = {xCoor, yCoor}
-      results.push(elemCoor);
     
-      document.querySelector(`.games[data-id="${i}"]`).style.left = `${xCoor - 50}px`;
-      document.querySelector(`.games[data-id="${i}"]`).style.top = `${yCoor - 50}px`;
-      document.querySelector(`.games[data-id="${i}"]`).classList.remove('hidden');
-    }
 
-  if(
-      e.target.classList.contains('fa-play')
-    ) {
-    startTimer();
-  } else if(e.target.classList.contains('fa-pause')) {
-    playBtn.classList.remove('hidden');
-    pauseBtn.classList.add('hidden');
-    clearInterval(downloadTimer);
+  // if(
+  //     e.target.classList.contains('fa-play')
+  //   ) {
+  //   startTimer();
+  // } else if(e.target.classList.contains('fa-pause')) {
+  //   playBtn.classList.remove('hidden');
+  //   pauseBtn.classList.add('hidden');
+  //   clearInterval(downloadTimer);
+  // }
+
+  placeImages('carrot', CARROT_COUNT, 'img/carrot.png');
+  placeImages('bug', BUG_COUNT, 'img/bug.png');
+
+  showStopButton();
+  timer.classList.remove('hidden');
+  score.classList.remove('hidden');
+}
+
+function placeImages(className, count, imgPath) {
+  for ( let i = 0; i < count; i++ ) {
+    const item = document.createElement('img');
+    item.setAttribute('class', className);
+    item.setAttribute('src', imgPath);
+    // minus carrot size from the max value so it doesn't go over the farmArea
+    const itemX = randomCor(farmAreaRect.x, farmAreaRect.width - CARROT_SIZE);
+    // set min value of itemY same as Rect.x because then the start point will be (8, 8) from left top corner.
+    const itemY = randomCor(farmAreaRect.x, farmAreaRect.height - CARROT_SIZE);
+    item.style.left = `${itemX}px`;
+    item.style.top = `${itemY}px`;
+    farmArea.appendChild(item);
   }
-  
 
 }
+
+function showStopButton() {
+  let buttonIcon = playPauseBtn.querySelector('.fa-play');
+  buttonIcon && buttonIcon.classList.add('fa-pause');
+  buttonIcon && buttonIcon.classList.remove('fa-play');
+  
+}
+
+function showPlayButton() {
+  let buttonIcon = playPauseBtn.querySelector('.fa-pause');
+  
+  buttonIcon.classList.add('fa-play');
+  buttonIcon.classList.remove('fa-pause');
+}
+
+
 function stopGame(clicked) {
 
   if( clicked == 'bug' ) {
@@ -147,8 +180,13 @@ function stopTimer() {
   clearInterval(downloadTimer);
 }
 
-playPauseBtn.addEventListener('click', initGame);
-//pauseBtn.addEventListener('click', pauseBtn.classList.add('paused'));
+playPauseBtn.addEventListener('click', () => {
+  initGame();
+  gameOn = !gameOn;
+
+} );
+
+
 rePlayBtn.forEach( btn => 
   btn.addEventListener('click', initGame )
 );
